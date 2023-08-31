d0e9b918de98ab830a21ad68b1252d69fe2f3b44
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2023-09-01 00:57:19 +0300

    remove unused imports

--- app/src/main/java/com/example/wikime/MainActivity.java
+++ app/src/main/java/com/example/wikime/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.wikime;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import tech.gusavila92.websocketclient.WebSocketClient;
 
 public class MainActivity extends AppCompatActivity {
 
