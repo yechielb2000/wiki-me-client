b2f565a9dd14f2c031ca100cfeb64ba755a745e1
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2023-09-01 00:05:16 +0300

    init

--- app/src/main/java/com/example/wikime/MainActivity.java
+++ app/src/main/java/com/example/wikime/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.wikime;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import tech.gusavila92.websocketclient.WebSocketClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createWebSocketClient();
+    }
+
+    private void createWebSocketClient() {
+        URI uri;
+        try {
+            uri = new URI("http://127.0.0.1:8000/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        WebSocketClient webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                Log.i("WebSocket", "Session is starting");
+                this.send("Hello World!");
+            }
+
+            @Override
+            public void onTextReceived(String s) {
+                Log.i("WebSocket", "Message received");
+                final String message = s;
+                runOnUiThread((Runnable) () -> {
+                    try {
+                        TextView textView = findViewById(R.id.animal_sound);
+                        textView.setText(message);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+
+            }
+
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived() {
+                Log.i("WebSocket", "Closed ");
+                System.out.println("onCloseReceived");
+            }
+        };
+
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
     }
 }
---
+++ app/src/main/java/com/example/wikime/socket/Socket.java
@@ -0,0 +1,21 @@
+package com.example.wikime.socket;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.wikime.R;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import java.net.URI;
+import java.net.URISyntaxException;
+import tech.gusavila92.websocketclient.WebSocketClient;
+import tech.gusavila92.websocketclient.WebSocketClient;
+
+public class Socket {
+
+}
