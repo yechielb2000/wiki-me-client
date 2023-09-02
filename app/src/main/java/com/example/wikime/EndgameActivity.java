9254b3ff60d2573cad2fe706a804e87f6b37bf7a
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2023-09-02 21:28:30 +0300

    create endgame activity

---
+++ app/src/main/java/com/example/wikime/EndgameActivity.java
@@ -0,0 +1,14 @@
+package com.example.wikime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EndgameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_endgame);
+    }
+}
